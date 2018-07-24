b53f16ce26e41b1c7ef685eeea372b3b0becd147
chevalier37 <editions@seconde-vie.fr>
2018-07-24 20:20:21 +0200

    notifications
    notifications

--- imports/ui/component/FormPosterReponse.js
+++ imports/ui/component/FormPosterReponse.js
@@ -92,7 +92,7 @@ class FormPosterReponse extends Component {
 		          //: ''
           	//}
 
-          	Meteor.call('serverNotification','Nouvelle réponse',titre, this.props.id)
+          	Meteor.call('serverNotification','Nouvelle réponse',titre, this.props.authorId)
 
 	}
 
